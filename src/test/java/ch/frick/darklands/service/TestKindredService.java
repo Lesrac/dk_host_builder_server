d28b7215239b8513a24b5d1abe99662aee67a930
Lesrac <daniel.frick88@gmail.com>
2016-09-19 22:50:39 +0200

    KindredService tested

--- src/test/java/ch/frick/darklands/service/TestKindredService.java
+++ src/test/java/ch/frick/darklands/service/TestKindredService.java
@@ -6,12 +6,14 @@ import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.never;
 
 import java.util.LinkedList;
 import java.util.List;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -28,24 +30,34 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import ch.frick.darklands.daos.KindredDAO;
 import ch.frick.darklands.data.Kindred;
 
+/**
+ * @author Daniel
+ *
+ */
 @RunWith(MockitoJUnitRunner.class)
 public class TestKindredService {
 	
 	private static List<Kindred> kindreds;
+	private static Kindred kindred;
 	private static String jsonKindreds;
+	private static String jsonKindred;
 	
 	private KindredService kindredService;
-	@Mock private KindredDAO kindredDAO;
-	
+	@Mock
+	private KindredDAO kindredDAO;
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		kindreds = new LinkedList<>();
-		kindreds.add(new Kindred("Fomoraic"));
+		kindred = new Kindred("Fomoraic");
+		kindred.setId(1l);
+		kindreds.add(kindred);
 		kindreds.add(new Kindred("Erainn"));
 		kindreds.add(new Kindred("Norse"));
 		kindreds.add(new Kindred("Infernii"));
 		ObjectMapper mapper = new ObjectMapper();
 		jsonKindreds = mapper.writeValueAsString(kindreds);
+		jsonKindred = mapper.writeValueAsString(kindred);
 	}
 
 	@AfterClass
@@ -56,6 +68,7 @@ public class TestKindredService {
 	public void setUp() throws Exception {
 		kindredDAO = mock(KindredDAO.class);
 		when(kindredDAO.getAll()).thenReturn(kindreds);
+		when(kindredDAO.getById(1l)).thenReturn(kindred);
 		kindredService = new KindredService(kindredDAO);
 	}
 
@@ -64,11 +77,11 @@ public class TestKindredService {
 	}
 
 	@Test
-	public void test() {
+	public void testGetAllKindreds() {
 		try {
 			Response response = kindredService.getAllKindreds();
 			assertNotNull(response);
-			assertEquals(200, response.getStatus());
+			assertEquals(Status.OK.getStatusCode(), response.getStatus());
 			assertEquals(response.getHeaderString("Content-Type"), MediaType.APPLICATION_JSON);
 			assertEquals(response.getEntity(), jsonKindreds);
 			verify(kindredDAO).getAll();
@@ -77,4 +90,27 @@ public class TestKindredService {
 		}
 	}
 
+	@Test
+	public void testGetKindredById() {
+		try {
+			Response response = kindredService.getKindredById(0l);
+			assertNotNull(response);
+			System.out.println(response);
+			assertEquals(Status.INTERNAL_SERVER_ERROR.getStatusCode(), response.getStatus());
+			assertEquals(response.hasEntity(), false);
+			assertEquals(response.getHeaderString("Content-Type"), null);
+			verify(kindredDAO, never()).getById(0l);
+			
+			response = kindredService.getKindredById(1l);
+			assertNotNull(response);
+			System.out.println(response);
+			assertEquals(Status.OK.getStatusCode(), response.getStatus());
+			assertEquals(response.getHeaderString("Content-Type"), MediaType.APPLICATION_JSON);
+			assertEquals(response.getEntity(), jsonKindred);
+			verify(kindredDAO).getById(1l);
+		} catch (JsonProcessingException e) {
+			fail("Exception happened: " + e.getMessage());
+		}
+	}
+	
 }
