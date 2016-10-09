bc5912bf6e93e056be9b5754347a4efebab3e226
Lesrac <daniel.frick88@gmail.com>
2016-10-09 13:39:18 +0200

    Added TestSuite

---
+++ src/test/java/ch/frick/darklands/service/AllServiceTests.java
@@ -0,0 +1,11 @@
+package ch.frick.darklands.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestKindredService.class, TestWarriorService.class })
+public class AllServiceTests {
+
+}
